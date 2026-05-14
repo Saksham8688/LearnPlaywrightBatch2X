0e8b5bed39a84432e2d42d7438018953bac8c6bf
SAKSHAM SINGH <sakshamsingh0930@gmail.com>
2026-05-14 17:49:53 +0530

    Initial commit: chapter 01 basics

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,1 @@
+console.log("Hello TO The Testing Academy");
---
+++ chapter_01_Basics/03_JS_Commands.js
@@ -0,0 +1,9 @@
+console.log(process.platform);
+//mac - darwin
+//windows - win32
+//linux - linux
+
+console.log(process.arch);
+//x64 - 64 bit
+//ia32 - 32 bit
+//arm64 - 64 bit arm
